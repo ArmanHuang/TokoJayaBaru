bdaa43fec1ad14d620f5a82054114e39ee3edde2
armanhuang <darkguardianz38@gmail.com>
2025-06-22 12:12:32 +0700

    revisi

--- landingpage/templatetags/custom_filters.py
+++ landingpage/templatetags/custom_filters.py
@@ -5,4 +5,11 @@ register = template.Library()
 
 @register.filter(name='get_item')
 def get_item(dictionary, key):
-    return dictionary.get(key) or static('images/default.png')
+    # Pastikan dictionary bukan None
+    if not dictionary:
+        return static('images/default.png')
+    # Ambil nilai, jika None atau kosong, pakai default
+    value = dictionary.get(key)
+    if not value:
+        return static('images/default.png')
+    return value
