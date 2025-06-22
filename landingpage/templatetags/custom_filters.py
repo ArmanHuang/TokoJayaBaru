1142e4e9bb94e284de08ef60d38aa6ef86123a72
armanhuang <darkguardianz38@gmail.com>
2025-06-22 11:53:16 +0700

    Add requirements, nixpacks config, runtime

--- landingpage/templatetags/custom_filters.py
+++ landingpage/templatetags/custom_filters.py
@@ -1,7 +1,8 @@
 from django import template
+from django.templatetags.static import static
 
 register = template.Library()
 
 @register.filter(name='get_item')
 def get_item(dictionary, key):
-    return dictionary.get(key, 'default.png')  
+    return dictionary.get(key) or static('images/default.png')
